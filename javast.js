c0ce2abec7a5fb0f7340bddc9917a1cdb6954a9f
AhmaduLo <gayeahmadubamba@gmail.com>
2021-08-19 19:17:52 +0200

    finish

---
+++ javast.js
@@ -0,0 +1,6 @@
+let menu= document.querySelector('.sidebar-menu');
+let sidebar= document.querySelector('.sidebar');
+
+sidebar.addEventListener("click", function(){
+    menu.classList.toggle('active')
+})
